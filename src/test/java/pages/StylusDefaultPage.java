ce6b6c945a2579c0395c23fd19d6502626eaf39b
Valeriy Salmin <clickua8@gmail.com>
2015-07-31 14:02:49 +0300

    test

--- src/test/java/pages/StylusDefaultPage.java
+++ src/test/java/pages/StylusDefaultPage.java
@@ -40,14 +40,16 @@ public class StylusDefaultPage extends TestBase {
         searchButton.click();
     }
 
-//    public void openPage(){
-//        driver.findElement(By.xpath("//a[@href='http://stylus.com.ua/apple-iphone-6/']")).click();
-//    }
+    public void openMenu() {
+        Actions action = new Actions(driver);
+        action.moveToElement(driver.findElement(By.xpath("//a[@href='http://stylus.com.ua/ru/apple_store/index.html']"))).perform();
+    }
 
-//    public void goToIphone6(){
-//        driver.findElement(By.xpath("//a[@href='http://stylus.com.ua/apple-iphone-6/']")).click();
-//        Assert.assertEquals(driver.getCurrentUrl(), URL);
-//    }
+    public void goTo(String product){
+        webDriverWait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//span[@class='title' and text()='"+product+"']/.."))));
+        Actions action = new Actions(driver);
+        action.moveToElement(driver.findElement(By.xpath("//span[@class='title' and text()='"+product+"']/.."))).click().perform();
+    }
 
     public void setLowPrice(String lowPrice) {
         driver.findElement(By.id("min")).clear();
@@ -61,20 +63,5 @@ public class StylusDefaultPage extends TestBase {
 
     public void confirm() {
         driver.findElement(By.xpath("//input[@type='submit' and @value='OK']")).click();
-    }
-
-    public void openMenu() {
-        Actions action = new Actions(driver);
-        action.moveToElement(driver.findElement(By.xpath("//a[@href='http://stylus.com.ua/ru/apple_store/index.html']"))).perform();
-    }
-
-    public void goTo(String product){
-//        webDriverWait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//a[@href='http://stylus.com.ua/apple-iphone-6/']"))));
-//        Actions action = new Actions(driver);
-//        action.moveToElement(driver.findElement(By.xpath("//a[@href='http://stylus.com.ua/apple-iphone-6/']"))).click().perform();
-
-        webDriverWait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//span[@class='title' and text()='"+product+"']/.."))));
-        Actions action = new Actions(driver);
-        action.moveToElement(driver.findElement(By.xpath("//span[@class='title' and text()='"+product+"']/.."))).click().perform();
     }
 }
